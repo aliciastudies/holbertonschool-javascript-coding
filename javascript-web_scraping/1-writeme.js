d79d75565d02be018e98501ce6d96a8472e8a3fd
aliciastudies <6722@holbertonstudents.com>
2024-01-22 16:26:35 +1100

    feat: Write me

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+// get file path from cmd line args
+const filePath = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(
+  filePath,
+  content,
+  {
+    encoding: 'utf8',
+    flag: 'w',
+    mode: 0o666,
+  },
+  (error) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.log(content);
+  },
+);
