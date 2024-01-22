2f9743654afdda8a9a1589ef0282d226d7614532
aliciastudies <6722@holbertonstudents.com>
2024-01-22 16:11:25 +1100

    feat: Readme

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+// get file path from cmd line args
 const filePath = process.argv[2];
 fs.readFile(filePath, 'utf8', (error, content) => {
   if (error) {
