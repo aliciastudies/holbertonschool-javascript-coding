98a3ee30124e1da4fa8f31e967af9dff4cc2c3ba
aliciastudies <6722@holbertonstudents.com>
2024-01-23 14:27:52 +1100

    feat: Star wars Wedge Antilles

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+const character = '18';
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  try {
+    const movieData = JSON.parse(body).results;
+    let count = 0;
+    for (const movie  of movieData) {
+        for (const char of movie.characters) {
+            if (char.includes(`${character}`)) {
+                count = count + 1;
+            }
+        }
+    }
+    console.log(count);
+  } catch (parseError) {
+    console.log(`Error parsing JSON: ${parseError}`);
+  }
+});
