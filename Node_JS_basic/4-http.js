f21634156544251a28dd276d0e5dedd1222f883c
aliciastudies <6722@holbertonstudents.com>
2024-02-04 20:11:29 +1100

    feat: Create a small HTTP server using the HTTP module

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const port = 1245;
+
+const app = http
+  .createServer((request, response) => {
+    response.write('Hello Holberton School!');
+    response.end();
+  })
+  .listen(port);
+
+module.exports = app;
