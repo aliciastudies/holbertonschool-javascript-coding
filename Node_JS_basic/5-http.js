dc83c4afbe3baec149651db0684e94c3823d46b9
aliciastudies <6722@holbertonstudents.com>
2024-02-05 13:32:06 +1100

    fix: added return for countStudents()

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -20,24 +20,17 @@ async function countStudents(filePath) {
         studentsSWE.push(firstname);
       }
     }
-    console.log(
-      `Number of students: ${studentsCS.length + studentsSWE.length}`,
-    );
-    console.log(
-      `Number of students in CS: ${studentsCS.length}. List: ${studentsCS.join(
-        ', ',
-      )}`,
-    );
-    console.log(
-      `Number of students in SWE: ${
-        studentsSWE.length
-      }. List: ${studentsSWE.join(', ')}`,
-    );
+    return `Number of students: ${
+      studentsCS.length + studentsSWE.length
+    }\n Number of students in CS: ${studentsCS.length}. List: ${studentsCS.join(
+      ', ',
+    )}\n Number of students in SWE: ${
+      studentsSWE.length
+    }. List: ${studentsSWE.join(', ')}`;
   } catch (error) {
     throw new Error('Cannot load the database');
   }
 }
-
 const port = 1245;
 
 const app = http
@@ -47,10 +40,10 @@ const app = http
     } else if (request.url === '/students') {
       try {
         const databaseName = await countStudents(process.argv[2]);
-        response.write(`This is the list of our students\n ${databaseName}`);
+        response.write(`This is the list of our students\n${databaseName}`);
         response.end();
       } catch (error) {
-        response.write(`This is the list of our students\n ${error.message}`);
+        response.write(`This is the list of our students\n${error.message}`);
         response.end();
       }
     }
