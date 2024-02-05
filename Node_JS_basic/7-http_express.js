f153cbd86b7129f01e8f82afd620e76e477e0cbf
aliciastudies <6722@holbertonstudents.com>
2024-02-05 14:08:20 +1100

    feat: Create a more complex HTTP server using Express

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const { request } = require('http');
+
+const app = express();
+const port = 1245;
+const fs = require('fs').promises;
+
+async function countStudents(filePath) {
+  try {
+    const fileContent = await fs.readFile(filePath, 'utf-8');
+    const studentLines = fileContent.split('\n').slice(1);
+
+    const studentsCS = [];
+    const studentsSWE = [];
+
+    for (const line of studentLines) {
+      const [firstname, , , field] = line.split(',');
+
+      if (field === 'CS') {
+        studentsCS.push(firstname);
+      }
+
+      if (field === 'SWE') {
+        studentsSWE.push(firstname);
+      }
+    }
+    return `Number of students: ${
+      studentsCS.length + studentsSWE.length
+    }\nNumber of students in CS: ${studentsCS.length}. List: ${studentsCS.join(
+      ', ',
+    )}\nNumber of students in SWE: ${
+      studentsSWE.length
+    }. List: ${studentsSWE.join(', ')}`;
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
+});
+
+app.get('/students', async (request, response) => {
+  try {
+    const databaseName = await countStudents(process.argv[2]);
+    response.send(`This is the list of our students\n${databaseName}`);
+  } catch (error) {
+    response
+      .status(500)
+      .send(`This is the list of our students\n${error.message}`);
+  }
+});
+
+app.listen(port);
+
+module.exports = app;
